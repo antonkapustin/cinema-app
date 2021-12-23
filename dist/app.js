e061df49d1b8c73e4761fa16f9898adaba37e62c
Anton Kapustin <85716128+antonkapustin@users.noreply.github.com>
2021-12-24 01:37:45 +0300

    Developer (#1)
    * create homepage
    
    * create footer to home page
    
    * update slider
    
    * update home page
    
    * create slider.ts
    
    * update slider.ts
    
    * update slider dotes
    
    * upgrate slider
    
    * webpack
    
    Co-authored-by: AntonKapustin <anton@MacBook-Pro-Toxachiz.local>

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,8 @@
-"use strict";
-console.log("HI");
+import { Slider } from "./components/slider/slider.component.js";
+const data = [
+  { name: "Joker", image: "assets/joker.jpeg", value: "1" },
+  { name: "vechnye", image: "assets/vechnye.jpeg", value: "2" },
+  { name: "encounter", image: "assets/encounter.jpeg", value: "3" },
+];
+const films = document.querySelector(".slider");
+const filmsSlider = new Slider(data, films);
