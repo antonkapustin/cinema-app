976424f111ab98b29b4610c7f71d400f2218fe6a
AntonKapustin <anton@MacBook-Pro-Toxachiz.local>
2021-12-24 01:36:25 +0300

    webpack

---
+++ webpack trying/src/app.ts
@@ -0,0 +1,13 @@
+import { Slider } from "./components/slider/slider.component";
+import { IData } from "./components/slider/slider.interfaces";
+import "./style.less";
+
+const data: IData[] = [
+  { name: "Joker", image: "assets/joker.jpeg", value: "1" },
+  { name: "vechnye", image: "assets/vechnye.jpeg", value: "2" },
+  { name: "encounter", image: "assets/encounter.jpeg", value: "3" },
+];
+
+const films = document.querySelector(".slider") as Element;
+
+const filmsSlider = new Slider(data, films);
