b818d94750371435b41b738d6bad3e615dbfdda4
AntonKapustin <anton@MacBook-Pro-Toxachiz.local>
2021-12-27 03:02:35 +0300

    create webpack for details

---
+++ webpack/webpack.config.js
@@ -0,0 +1,106 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const OptimizeCssWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: "all",
+    },
+  };
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ];
+  }
+
+  return config;
+};
+//"@babel/polyfill"
+const fileName = (ext) => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: ["./app.ts"],
+  output: {
+    filename: fileName("js"),
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".less"],
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 4200,
+    hot: isDev,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new HTMLWebpackPlugin({
+      filename: "details.html",
+      template: "./details/details.html",
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./src/assets"),
+          to: path.resolve(__dirname, "./dist/assets"),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: fileName("css"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-typescript"],
+          },
+        },
+      },
+      { test: /\.(png|jpg|svg|gif)$/, use: ["file-loader"] },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        loader: "file-loader",
+        options: {
+          outputPath: "../fonts",
+        },
+      },
+    ],
+  },
+};
